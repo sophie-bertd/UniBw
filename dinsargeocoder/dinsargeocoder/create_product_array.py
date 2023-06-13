31aea63d78a532389692929a9a8f3a4f8e0b2913
AlphardHydrae <sophie_bertrand@zohomail.eu>
2023-06-13 17:01:30 +0200

    [UPDATE] updated documentation and comments

--- dinsargeocoder/dinsargeocoder/create_product_array.py
+++ dinsargeocoder/dinsargeocoder/create_product_array.py
@@ -1,4 +1,11 @@
 import snappy
 
 def create_product_array(products):
+    """
+    Creates a Java array of SnapPy products
+
+    :param products: Python list of products
+    :return: Java array of products
+    """
+
     return snappy.jpy.array('org.esa.snap.core.datamodel.Product', products)
--- dinsargeocoder/dinsargeocoder/write_dim_product.py
+++ dinsargeocoder/dinsargeocoder/write_dim_product.py
@@ -1,5 +1,13 @@
 import snappy
 
 def write_dim_product(source, save_path):
+    """
+    Writes a SNAP product to disk in DIMAP format
+
+    :param source: SNAP product
+    :param save_path: Path to save the product
+    :return: None
+    """
+    
     write_format = 'BEAM-DIMAP'
     snappy.ProductIO.writeProduct(source, save_path, write_format)

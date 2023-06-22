09edd752679bf116486bd9ba03621f9925e52b39
AlphardHydrae <sophie_bertrand@zohomail.eu>
2023-06-22 10:29:53 +0200

    [UPDATE] updated dependencies

--- dinsargeocoder/setup.py
+++ dinsargeocoder/setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='dinsargeocoder',
     packages=find_packages(),
-    version='3.2.1',
+    version='3.3.1',
     description='DInSAR Geocoder',
     author='Francescopaolo Sica, Bertrand Sophie',
     license='MIT',
-    install_requires=['snappy', 'numpy', 'rasterio', 'gdal', 'matplotlib'],
+    install_requires=['snappy'],
 )
